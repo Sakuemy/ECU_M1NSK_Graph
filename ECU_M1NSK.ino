7d408814bd1017b145a0ab990db48b31101c02f1
root <root@skuemypc.localdomain>
2023-01-23 17:20:24 +0300

    Add new version

--- ECU_M1NSK.ino
+++ ECU_M1NSK.ino
@@ -2,23 +2,25 @@ unsigned long time = 0;
 unsigned long time2 = 0;
 int engineSpeed = 0;
 bool b;
-char val;
-char valSave;
-const int buttonPin = 5;     // the number of the pushbutton pin
-int buttonState = 0;         // variable for reading the pushbutton status
+const int buttonPin = 5;			// Номер пина для определения количества оборотов мотора в минуту
+int IgnitionState = 0;				// Сохранение статуса пина buttonPin
+float voltage = 0.0;
+float temp=0.0;						// Переменная для преобразования напряжения
 
 void setup(){
   Serial.begin(9600);
-  Serial.setTimeout(50); // установить таймаут
-  pinMode(13, OUTPUT); // 13 пин – светодиод, объявляется как выход
-  pinMode(buttonPin, INPUT);
+  Serial.setTimeout(50);
+  pinMode(13, OUTPUT); // 13 пин – светодиод, объявляется как выход для тестов работы
+  pinMode(buttonPin, INPUT); // подкллючение пина для определение оборотов
+  float r1=100000.0; //сопротивление резистора r1
+  float r2=10000.0; // сопротивление резистора r2
 }
 
 void loop(){
-  buttonState = digitalRead(buttonPin);
+  IgnitionState = digitalRead(buttonPin);
   
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if ((buttonState == HIGH) and (b == false)) {
+  // Определение количества оборотов мотора в минуту
+  if ((IgnitionState == HIGH) and (b == false)) {
     if (engineSpeed != 0){
         engineSpeed = round(((60000 / (millis() - time2)) + engineSpeed) / 2);
     } else {
@@ -27,34 +29,22 @@ void loop(){
     time2 = millis();
     b = true;
   }
-  if (buttonState == LOW) {
+  if (IgnitionState == LOW) {
     b = false;
   }
+  
+  // Определение вольтажа
+  temp = (analogRead(A3) * 5.0) / 1024.0; // формула для конвертирования значения напряжения
+  voltage = temp / (r2/(r1+r2)); // определение напряжения
+  if (voltage < 0.1)
+  {
+    voltage=0.0;
+  }
 
-
-  if ((millis() - time > 1000)){ // and (valSave == '2')
+  // Передача значения
+  if ((millis() - time > 1000)){
       time = millis();
-      Serial.println(String(engineSpeed) + ";" + String(analogRead(A4)) + ";" + String(analogRead(A3)) + ";");
+      Serial.println(String(engineSpeed) + ";" + String(analogRead(A4)) + ";" + String(voltage) + ";");  // Передача данных по serial в виде "обороты;температура;вольтаж;"
       engineSpeed = 0;
   }
-/*
-  if ((Serial.available() > 0)) {
-    val = Serial.read();
-    switch (val) {
-      case '1':
-        Serial.print(engineSpeed);
-        engineSpeed = 0;
-        break;
-      case '2':
-        Serial.print(analogRead(A4));
-        break;
-      case '3':
-        Serial.print(analogRead(A2));
-        break;
-      case '0':
-        Serial.print(0);
-        break;
-    }
-  }
-*/
 }
