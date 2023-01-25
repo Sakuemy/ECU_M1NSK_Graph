0d589da62b7f51b785645237b726ebb7ffa98833
root <root@skuemypc.localdomain>
2023-01-25 14:03:14 +0300

    New version 0.6

--- ECU_M1NSK.ino
+++ ECU_M1NSK.ino
@@ -2,22 +2,22 @@ unsigned long time = 0;
 unsigned long time2 = 0;
 int engineSpeed = 0;
 bool b;
-const int buttonPin = 5;			// Номер пина для определения количества оборотов мотора в минуту
+const int buttonPin = 17;			// Номер пина для определения количества оборотов мотора в минуту
 int IgnitionState = 0;				// Сохранение статуса пина buttonPin
 float voltage = 0.0;
 float temp=0.0;						// Переменная для преобразования напряжения
+float r1=10000.0; //сопротивление резистора r1
+float r2=3330.0; // сопротивление резистора r2
 
 void setup(){
   Serial.begin(9600);
   Serial.setTimeout(50);
   pinMode(13, OUTPUT); // 13 пин – светодиод, объявляется как выход для тестов работы
   pinMode(buttonPin, INPUT); // подкллючение пина для определение оборотов
-  float r1=100000.0; //сопротивление резистора r1
-  float r2=10000.0; // сопротивление резистора r2
 }
 
 void loop(){
-  IgnitionState = digitalRead(buttonPin);
+/*  IgnitionState = digitalRead(buttonPin);
   
   // Определение количества оборотов мотора в минуту
   if ((IgnitionState == HIGH) and (b == false)) {
@@ -26,15 +26,31 @@ void loop(){
     } else {
       engineSpeed = round(60000 / (millis() - time2));
     }
+    engineSpeed = abs(engineSpeed);
     time2 = millis();
     b = true;
   }
   if (IgnitionState == LOW) {
     b = false;
   }
+*/
+
+  if(analogRead(buttonPin)>400){                             //  если показания датчика линии больше 400 (тёмное поле)
+    if(time2<millis()){                                  //  если нет переполнения millis(), то ...
+      engineSpeed = 60000/(millis()-time2);             //  записываем данные по оборотам
+    }
+    time2 = millis();                                  //  присваиваем переменной time2 текущее значение millis();
+    while(analogRead(buttonPin)>350){                        //  ждём пока датчик линии не выйдет из тёмного поля
+      if((millis()-time2)>1000){
+		  engineSpeed = 0.0;
+	  }  //  если датчик находится на тёмном  поле дольше 6 секунд, то выводим 0.0
+    }
+  }   if((millis()-time2)>1000){
+		  engineSpeed = 0.0;
+	  }	  //  если датчик находится на светлом поле дольше 6 секунд, то выводим 0.0
   
   // Определение вольтажа
-  temp = (analogRead(A3) * 5.0) / 1024.0; // формула для конвертирования значения напряжения
+  temp = (analogRead(A2) * 5.0) / 1024.0; // формула для конвертирования значения напряжения
   voltage = temp / (r2/(r1+r2)); // определение напряжения
   if (voltage < 0.1)
   {
@@ -44,7 +60,7 @@ void loop(){
   // Передача значения
   if ((millis() - time > 1000)){
       time = millis();
-      Serial.println(String(engineSpeed) + ";" + String(analogRead(A4)) + ";" + String(voltage) + ";");  // Передача данных по serial в виде "обороты;температура;вольтаж;"
-      engineSpeed = 0;
+      
+      Serial.println(String(abs(round(engineSpeed / 2))) + ";" + String("0.0") + ";" + String(voltage) + ";");  // Передача данных по serial в виде "обороты;температура;вольтаж;"
   }
 }
